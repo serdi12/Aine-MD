15cc3162d10549a111802a59698c571e80e06e88
serdi12 <109755111+serdi12@users.noreply.github.com>
2022-07-29 08:54:33 +0700

    Update and rename plugins/sewabot.js to plugin/sewabot.js

--- plugin/sewabot.js
+++ plugin/sewabot.js
@@ -3,7 +3,7 @@ let handler = async (m, { conn }) => {
   let pepe = 'https://telegra.ph/file/2e2eb7d093fec08fd00d4.jpg'
   let baper = await fetch(pepe).then(a => a.buffer())
 
-  let aine = '6289654360447@s.whatsapp.net'
+  let aine = '6289516947204@s.whatsapp.net'
   let a = await conn.profilePictureUrl(conn.user.jid, 'image').catch((_) => "https://telegra.ph/file/24fa902ead26340f3df2c.png")
   let b = await conn.profilePictureUrl(owner[0]+'@s.whatsapp.net', 'image').catch((_) => "https://telegra.ph/file/24fa902ead26340f3df2c.png")
   let c = pickRandom([a, b])
@@ -13,15 +13,15 @@ let handler = async (m, { conn }) => {
   description: ` *• SEWA BOT & UP TO PREMIUM •*
         
 1. Grup / 30 Hari
-Rp. 15.000 Dana
+Rp. 15.000 Gopay 
 Rp. 15.000 Pulsa
 
 2. Premium / 30 Hari
-Rp. 10.000 Dana
+Rp. 10.000 Gopay 
 Rp. 10.000 Pulsa
 
 3. Premium + Grup / 30 Hari
-Rp. 20.000 Dana
+Rp. 20.000 Gopay 
 Rp. 20.000 Pulsa
 
 wa.me/${owner[0]}
@@ -36,9 +36,9 @@ wa.me/${owner[0]}
   products:[{productId:'7487741964584083'}]}],
   headerImage: { productId: '7487741964584083',
   jpegThumbnail: baper },
-  businessOwnerJid: `6289654360447@s.whatsapp.net`
+  businessOwnerJid: `6289516947204@s.whatsapp.net`
   },
-  footerText: 'https://aiinne.github.io',
+  footerText: 'https://saweria.co/IyanMd',
   }},{})
   conn.relayMessage(prepare.key.remoteJid,prepare.message,{messageId:prepare.key.id})
   const data = global.owner.filter(([id, isCreator]) => id && isCreator)
