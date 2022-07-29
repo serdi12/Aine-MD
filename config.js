268c820f34c88acbb19c890d452f7a52db7be0b5
serdi12 <109755111+serdi12@users.noreply.github.com>
2022-07-29 08:46:01 +0700

    Update config.js

--- config.js
+++ config.js
@@ -2,9 +2,9 @@ let fs = require('fs')
 let chalk = require('chalk')
 
 owner = [
-  ['62895330379186'],
-  ['62895330379186'],
-  ['62895330379186', 'Owner', true]
+  ['6289516947204'],
+  ['6289516947204'],
+  ['6289516947204', '𝚒𝚢𝚊𝚗Ⓒ︎', true]
   // [number, dia creator/owner?, dia developer?]
 ] // Put your number here
 mods = [] // Want some help?
@@ -40,8 +40,8 @@ process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = 0
 const spack = fs.readFileSync("lib/exif.json")
 const stickerpack = JSON.parse(spack)
 if (stickerpack.spackname == '') {
-  var sticker_name = '@ainee_bot'
-  var sticker_author = 'Aine'
+  var sticker_name = '@𝚁𝙲𝚃𝙸 𝙱𝙾𝚃'
+  var sticker_author = '𝚒𝚢𝚊𝚗'
 } else {
   var sticker_name = stickerpack.spackname
   var sticker_author = stickerpack.sauthor
@@ -58,7 +58,7 @@ fs.watchFile(file_exif, () => {
 // Sticker WM
 packname = sticker_name
 author = sticker_author
-wm = '© Aine'
+wm = '© 𝚒𝚢𝚊𝚗'
 
 multiplier = 1000 // The higher, The harder levelup
 
